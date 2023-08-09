96e12e58a6b71d20a3af8ca244d39a26b925249b
Occamies <lasseknudsen01@hotmail.com>
2023-08-09 09:40:18 +0200

    worldcruise first

---
+++ src/app/navknap/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import variables from "./variables.module.scss"
+
+const page = () => {
+  return (
+    <h1 style={{color:variables.primaryColor}}>hello. next.js</h1>
+  )
+}
+
+export default page
