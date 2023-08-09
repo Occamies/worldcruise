8e72c044713adbb2249048d7240d8a1e7c9d47e7
Occamies <lasseknudsen01@hotmail.com>
2023-08-09 14:02:55 +0200

    start navbar

---
+++ src/app/navbar/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from "@/style/style.module.scss"
+
+const page = () => {
+  return (
+    <>
+    <main  className={style.main}>
+    <section className={style.navbarcontainer}>
+      <img src="" alt="icon" />
+      <input type="text" />
+      flag
+    </section>
+    </main>
+    </>
+  )
+}
+
+export default page
