23b0ad1545cb1e53b6740d4900fbc450388e011f
Jelocin123 <nicolej.basse123@gmail.com>
2023-08-10 12:09:48 +0200

    update

---
+++ src/app/productsubpage/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Map from '@/components/map'
+import Navbarsubpage from '@/components/navbarsubpage'
+import SliderPage from '@/components/slider'
+
+function page() {
+  return (
+    <>
+    <Navbarsubpage/>
+    <Map />
+    <SliderPage />
+    </>
+  )
+}
+
+export default page
---
+++ src/components/map.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function map() {
+  return (
+    <>
+    <div className='col-12'>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d17780.34890045502!2d10.169232629094934!3d56.147705048012824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smad!5e0!3m2!1sda!2sdk!4v1691654057850!5m2!1sda!2sdk" width="100%" height="650" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
+</>
+ 
+  )
+}
+
+export default map
