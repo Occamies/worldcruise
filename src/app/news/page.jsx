481cb9068559b5fd5f3d2cfca8c646c8c5318ff3
Occamies <lasseknudsen01@hotmail.com>
2023-08-10 13:08:23 +0200

    newspage

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Newsbox from '../../components/newsbox'
+import Navbarsubpage from '@/components/navbarsubpage'
 
 const page = () => {
   return (
     <div>
+      <Navbarsubpage/>
       <Newsbox />
     </div>
   )
--- src/components/newsbox.jsx
+++ src/components/newsbox.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import style from '@/style/style.module.scss'
 
 const newsbox = () => {
   return (
-    <div>
-      
+    <div className={style.newscon}>
+      <img src="/assets/stock1.jpeg" alt="news img" className={style.newsimg} />
+      <div>
+        <h2>Lorem ipsum dolor sit.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem modi animi voluptates debitis ea sequi! Assumenda ut dolor laboriosam mollitia. <a href="#">læs mere...</a></p>
+       
+      </div>
     </div>
   )
 }
