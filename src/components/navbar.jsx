a2fcb6d386ad471afaf49bbec51874c4f6bce846
Occamies <lasseknudsen01@hotmail.com>
2023-08-10 10:24:25 +0200

    navbar done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import style from "@/style/style.module.scss"
 
 
-const page = () => {
+const navbar = () => {
   return (
     <>
       <section className={style.navbarcontainer}>
@@ -21,4 +21,4 @@ const page = () => {
   )
 }
 
-export default page
+export default navbar
