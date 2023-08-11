1ce5fd35c807c099058d705bf4f8092077932fd0
Emil Blichfeldt <eb@limecode.dk>
2023-08-11 10:24:33 +0200

    update

---
+++ src/app/infoscreen/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Info from '../../components/infoScreen'
+
+const page = () => {
+  return (
+    <div>
+      <Info />
+    </div>
+  )
+}
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client"
+import React, { useState, useEffect } from "react";
 import style from '../style/homepage.module.scss'
 
 //import components
@@ -6,8 +8,35 @@ import Card from '../components/card'
 import Slider from '../components/slider'
 import News from '@/components/News'
 import Footer from '../components/footer'
+import Modal from '../components/modal'
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const openModal = (event) => {
+    setOpen(true);
+    setIndex(event.currentTarget.dataset.index);
+  }
+  const closeModal = () => {
+    setOpen(false);
+
+    onComplete: () => {
+      setOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOpen(true);
+    }, 15000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+
   return (
     <main className={style.main}>
       <Navbar />
@@ -15,6 +44,11 @@ export default function Home() {
       <Slider />
       <News />
       <Footer />
+
+      <Modal
+        isOpen={open}
+        onClose={closeModal}
+      />
     </main>
   )
 }
